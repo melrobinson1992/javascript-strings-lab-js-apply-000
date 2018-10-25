a5323be45dc4b2638b8afd47a3166df1f87ae9ef
Melanie Robinson <melrobinson1992@yahoo.com>
2018-10-25 02:33:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "Hello," + specialGuest + "!";
+var greetSpecialGuest = "" + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = `Let\'s talk about ${topic}`;
+var conversation = `${topic}`;
